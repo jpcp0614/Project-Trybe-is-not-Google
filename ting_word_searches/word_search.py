946eb69ddb98bc00bb9fbbab7ae52e1b2a2495c0
jpcp0614 <jpcp0614@gmail.com>
2022-08-08 12:44:39 -0300

    :sparkles: feat: Requisito 06

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,28 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    occurrences = list()
+
+    for element in instance.list:
+        occurrences_lines = list()
+
+        occurrences.append({
+            'palavra': word,
+            'arquivo': element['nome_do_arquivo'],
+            'ocorrencias': occurrences_lines
+        })
+
+        for index, line in enumerate(element['linhas_do_arquivo']):
+            word_lower = word.lower()
+            line_lower = line.lower()
+
+            if word_lower in line_lower:
+                occurrences_lines.append({
+                    'linha': index + 1
+                })
+
+        if len(occurrences_lines) == 0:
+            occurrences.pop()
+
+    return occurrences
 
 
 def search_by_word(word, instance):
