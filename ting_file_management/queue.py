81d5d4f8f6782f0c7a678660dee5c3b7b07fd2f4
jpcp0614 <jpcp0614@gmail.com>
2022-08-08 11:31:09 -0300

    :sparkles: feat: Requisito 01

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,17 @@
 class Queue:
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.list = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.list)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        return self.list.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.list.pop(0)  # topo do lista
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index > len(self.list):
+            raise IndexError
+        return self.list[index]
